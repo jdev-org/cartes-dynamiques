1650884adda249ebfee1e13285a192841e002d8f
Agath21 <agathe.adam@jdev.fr>
2023-07-27 16:46:32 +0200

    Update map fontaines

---
+++ fontaines/customlayers/commerces.js
@@ -0,0 +1,23 @@
+
+const LAYER_URL = 'https://edp.jdev.fr/geoserver/edp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=commerce&outputFormat=application%2Fjson';
+const LAYER_ID = "commerces";
+
+var commerceStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+        src: `apps/fontaines/img/commerces.svg`,
+        scale: 0.45
+    })
+})
+
+let layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: LAYER_URL,
+        format: new ol.format.GeoJSON()
+    }),
+    style: commerceStyle
+});
+
+
+
+var densite_carcerale = new CustomLayer(LAYER_ID, layer, legend);
+
---
+++ fontaines/customlayers/fontaines.js
@@ -0,0 +1,51 @@
+
+const LAYER_URL = 'https://edp.jdev.fr/geoserver/edp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fontaines&outputFormat=application%2Fjson';
+const LAYER_ID = "fontaines";
+
+const dispoIconesPath = {
+    OUI: {src: `apps/fontaines/img/fontaine_disponible.svg`},
+    NON: {src: `apps/fontaines/img/fontaine_indisponible.svg`}
+};
+
+
+// create styles
+const uniqueStyle = (feature) => {
+    const props = feature.getProperties();
+    const dispo = props["dispo"];
+    const access = props["accessible"];
+    const type = props["type_objet"];
+    const dispoIcon = dispoIconesPath[dispo];    
+    let iconFontaine;
+    if(access == 'Non'){
+        iconFontaine = `apps/fontaines/img/fontaine_nonaccessible.svg`;
+    } else{
+        iconFontaine= dispoIcon?.src;
+    }
+    return [
+        new ol.style.Style({            
+            image: new ol.style.Icon({
+                scale: 0.45,
+                src: iconFontaine || `apps/fontaines/img/fontaine_nonaccessible.svg`,
+            })
+        }),
+        new ol.style.Style({     
+            image: new ol.style.Icon({
+                scale: 0.45,
+                src: `apps/fontaines/img/${type}.svg`,
+            })
+        })
+]};
+
+
+let layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: LAYER_URL,
+        format: new ol.format.GeoJSON()
+    }),
+    style: uniqueStyle
+});
+
+
+
+var fontaines = new CustomLayer(LAYER_ID, layer, legend);
+
