5c97415a0437c6e39cf4bdb4afa7aeb07d9cd0b5
Pierre Jégo <pierre.jego@jdev.fr>
2023-11-28 16:14:03 +0100

    Remove unneeded information

--- edp_qualite_eau/scripts/mviewer_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# Redéfition de l'adresse du Geoserver
-[ -n ${GEOSERVER_URL} ] && find /usr/share/nginx/html/apps -type f -name "default.xml" -exec sed -i "s/\[GEOSERVER_URL\]/${GEOSERVER_URL//\//\\/}/g" {} \;
-sed -i 's|80;|8081;|' /etc/nginx/conf.d/default.conf ;
-sh /docker-entrypoint.sh nginx -g "daemon off;"
--- fontaines_wallace/script.sql
+++
@@ -1,2 +0,0 @@
-alter table fontaine.fontaine 
-add url_jeu_vp VARCHAR (300);
