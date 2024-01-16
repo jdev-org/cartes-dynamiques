cf1b975e6d0befe0edc2f74f35827a3f4e2579d0
Agath21 <agathe.adam@jdev.fr>
2024-01-16 10:52:38 +0100

    Add date of data update for fountains and wallace #15

--- fontaines_wallace/customcontrol/fontaines.js
+++ fontaines_wallace/customcontrol/fontaines.js
@@ -1,30 +1,52 @@
 const layerid = "fontaines";
-const cc = (function() {
+const cc = (function () {
+  /*
+   * Private
+   */
 
+  var _initialized = false;
+
+  function getDateDataUpdate() {
+    var urlS = new URL(
+      "https://edp.jdev.fr/geoserver/edp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=edp%3Afontaines_maj&outputFormat=application%2Fjson"
+    );
+    $.getJSON(urlS, function (dataS) {
+      var itemsS = [];
+      $.each(dataS.features, function (key, valS) {
+        $.each(valS.properties, function (i, j) {
+          if (i == "datemaj") {
+            $("#section_date_maj").html(
+              "<span i18n='sidebar.update.data'>Date de dernière mise à jour</span>" +
+                " :<br> <b>" +
+                j +
+                "</b> "
+            );
+          } else {
+            itemsS.push("<li>" + j.replace(/,/gi, "</li><li>") + " ");
+          }
+        });
+      });
+    });
+  }
+
+  return {
     /*
-    * Private
-    */
+     * Public
+     */
 
-    var _initialized = false;
+    init: function () {
+      // mandatory - code executed when layer is added to legend panel
+      if (!_initialized) {
+        let divLegend = document.getElementById("legend-fontaines").parentNode;
+        $("#legendFontaines_wallace").insertAfter(divLegend);
+        getDateDataUpdate();
+      }
+    },
 
-    return {
-        /*
-        * Public
-        */
-
-        init: function () {
-            // mandatory - code executed when layer is added to legend panel
-            if (!_initialized) {
-                let divLegend = document.getElementById("legend-fontaines").parentNode;
-                $("#legendFontaines_wallace").insertAfter(divLegend); 
-            }
-        },
-
-        destroy: function () {
-            // mandatory - code executed when layer panel is closed
-            _initialized = false;
-        }
-    };
-
-}());
+    destroy: function () {
+      // mandatory - code executed when layer panel is closed
+      _initialized = false;
+    },
+  };
+})();
 new CustomControl(layerid, cc.init, cc.destroy);
