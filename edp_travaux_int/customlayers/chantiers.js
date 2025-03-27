d265883c15d0ce2fc477ed1ba873c92a60a1d60b
Agath21 <agathe.adam@jdev.fr>
2025-03-27 10:55:37 +0100

    Add edp config and layer for app #134

---
+++ edp_travaux_int/customlayers/chantiers.js
@@ -0,0 +1,88 @@
+let EPstyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: "rgba(170, 184, 255, 1)",
+  }),
+  stroke: new ol.style.Stroke({
+    color: "rgba(170, 184, 255, 1)",
+    width: 2,
+  }),
+});
+
+let ENPstyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: "rgba(255, 239, 170, 1)",
+  }),
+  stroke: new ol.style.Stroke({
+    color: "rgba(255, 239, 170, 1)",
+    width: 2,
+  }),
+});
+
+let INSTALL_RESstyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: "rgba(174, 255, 174, 1)",
+  }),
+  stroke: new ol.style.Stroke({
+    color: "rgba(174, 255, 174, 1)",
+    width: 2,
+  }),
+});
+
+let GENIE_CIVILstyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: "rgba(255, 199, 151, 1)",
+  }),
+  stroke: new ol.style.Stroke({
+    color: "rgba(255, 199, 151, 1)",
+    width: 2,
+  }),
+});
+
+let NullStyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: "rgba(226, 226, 226, 1)",
+  }),
+  stroke: new ol.style.Stroke({
+    color: "rgba(226, 226, 226, 1)",
+    width: 2,
+  }),
+});
+
+let chantiersLegend = { title: "Types de chantiers", items: [] };
+
+chantiersLegend.items.push({ styles: EPstyle, label: "EP", geometry: "Polygon" });
+chantiersLegend.items.push({ styles: ENPstyle, label: "ENP", geometry: "Polygon" });
+chantiersLegend.items.push({
+  styles: INSTALL_RESstyle,
+  label: "INSTALL_RES",
+  geometry: "Polygon",
+});
+chantiersLegend.items.push({
+  styles: GENIE_CIVILstyle,
+  label: "GENIE_CIVIL",
+  geometry: "Polygon",
+});
+chantiersLegend.items.push({ styles: NullStyle, label: "null", geometry: "Polygon" });
+
+const chantiersLayer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: "apps/edp_travaux/data/chantiers.geojson",
+    format: new ol.format.GeoJSON(),
+  }),
+  style: function (feature) {
+    let style;
+    if (feature.get("nature_chantier") === "EP") {
+      style = EPstyle;
+    } else if (feature.get("nature_chantier") === "ENP") {
+      style = ENPstyle;
+    } else if (feature.get("nature_chantier") === "INSTALL_RES") {
+      style = INSTALL_RESstyle;
+    } else if (feature.get("nature_chantier") === "GENIE_CIVIL") {
+      style = GENIE_CIVILstyle;
+    } else if (feature.get("nature_chantier") === null) {
+      style = NullStyle;
+    }
+    return style;
+  },
+});
+new CustomLayer("chantiers", chantiersLayer, chantiersLegend);
