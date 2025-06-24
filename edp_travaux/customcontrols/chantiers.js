3af3cbec3931325b0f521cb09734e8636070a325
raphael-dsc <raphael.de.sousa@jdev.fr>
2025-06-24 14:40:06 +0200

    fixed issue with update date not being set

--- edp_travaux/customcontrols/chantiers.js
+++ edp_travaux/customcontrols/chantiers.js
@@ -63,7 +63,7 @@ const cc = (function () {
   // Date de mise à jour
   async function getDateDataUpdate() {
     var url = new URL(
-      `${mviewer.env?.url}/geoserver/${mviewer.env?.namespace}/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=${mviewer.env?.namespace}%3A${mviewer.env?.dataUpdateLayer}&maxFeatures=50&outputFormat=application%2Fjson`
+      `${mviewer.env?.url}/geoserver/${mviewer.env?.namespace}/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=${mviewer.env?.dataUpdateLayer}%3Adonnees_maj&maxFeatures=50&outputFormat=application%2Fjson`
     );
     fetch(url)
       .then((response) => response.json())
@@ -76,6 +76,12 @@ const cc = (function () {
             }
           }
         });
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la récupération de la date de mise à jour :",
+          error
+        );
       });
   }
 
